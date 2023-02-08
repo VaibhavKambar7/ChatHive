5cf0012909ca4463b338a4716909a412df39166b
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-02-09 01:03:43 +0530

    fixing broken nested folder

---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Box, Container, Text } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
+const Homepage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        className="box"
+        display="flex"
+        justifyContent="center"
+        p={4} //padding
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="1g"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Work Sans"
+          fontWeight="bold"
+          color="black"
+        >
+          Chat-App
+        </Text>
+      </Box>
+
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign-Up</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
