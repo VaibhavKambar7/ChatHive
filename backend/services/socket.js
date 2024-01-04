936cd82c833fc839d0711d7e40c3ac7372e16706
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2024-01-05 01:47:28 +0530

    moved socket logic from server.js to socket.js

---
+++ backend/services/socket.js
@@ -0,0 +1,46 @@
+const { Server } = require("socket.io");
+
+const socketService = (server) => {
+  const io = new Server(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(socket.id);
+    console.log("connected to socket.io");
+
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User joined Room :- " + room);
+    });
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+
+      if (!chat.users) return console.log("chat.users not defined");
+
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
+
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+
+    socket.off("setup", (userData) => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
+    });
+  });
+};
+module.exports = socketService;
